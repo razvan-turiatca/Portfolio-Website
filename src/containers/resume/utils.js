f305e3590302bdd5a67ed9a01bc57e7e57203d10
razvan-turiatca <codingwithrazvan@gmail.com>
2023-01-17 22:44:06 +0200

    implement resume section

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,44 @@
+export const data = {
+  experience: [
+    {
+      project: 'Portfolio Website',
+      used: 'ReactJS, SCSS, CSS, NPM',
+    },
+    {
+      project: 'Flash Cards',
+      used: 'ReactJS, CSS, NPM',
+    },
+    {
+      project: 'Movie App',
+      used: 'Javascript, Typescript, CSS, Bootstrap, NPM, Webpack, API',
+    },
+    {
+      project: 'Weather App',
+      used: 'Javascript, CSS, API',
+    },
+    {
+      project: 'Snowing',
+      used: 'Javascript, CSS',
+    },
+  ],
+  education: [
+    {
+      title: 'Web Developer Advanced',
+      school: 'Boogeek.com',
+      description: 'Learned Typescript, React and Node',
+      date: 'Mar 2022 - Apr 2022',
+    },
+    {
+      title: 'Web Developer Intermediate',
+      school: 'Boogeek.com',
+      description: 'Learned Javascript, CSS, HTML',
+      date: 'Sep 2021 - Feb 2022',
+    },
+    {
+      title: 'International Business',
+      school: 'Universitatea AL I Cuza, Iasi',
+      description: 'Learned business',
+      date: 'Sep 2004 - Jun 2008',
+    },
+  ],
+}
