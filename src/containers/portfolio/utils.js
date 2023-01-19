042f565d7c85ac77292f9a78c6cf6bc1ef755a3b
razvan-turiatca <codingwithrazvan@gmail.com>
2023-01-20 00:10:29 +0200

    implement portfolio section

---
+++ src/containers/portfolio/utils.js
@@ -0,0 +1,50 @@
+import imageOne from '../../images/image1.jpg'
+import imageTwo from '../../images/image2.jpg'
+
+export const portfolioData = [
+  {
+    sectionId: 2,
+    project: 'Catch me game',
+    projectLink: '',
+    image: imageOne,
+  },
+  {
+    sectionId: 2,
+    project: 'flake',
+    projectLink: '',
+    image: imageTwo,
+  },
+  {
+    sectionId: 2,
+    project: 'weather app',
+    projectLink: '',
+    image: imageOne,
+  },
+  {
+    sectionId: 3,
+    project: 'flash cards',
+    projectLink: '',
+    image: imageTwo,
+  },
+  {
+    sectionId: 3,
+    project: 'portfolio website',
+    projectLink: '',
+    image: imageOne,
+  },
+]
+
+export const filterOptions = [
+  {
+    label: 'All',
+    id: 1,
+  },
+  {
+    label: 'Javascript',
+    id: 2,
+  },
+  {
+    label: 'React',
+    id: 3,
+  },
+]
